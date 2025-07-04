70854b69a0c75f6720631ced5fbcfbb558e8352e
Kevin Robert Stravers <kevin@stravers.net>
2025-07-04 18:33:53 -0400

    More edits

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello world" << std::endl;
     int x = 0;
     x += 1;
+    std::cout << "Argument count: " << argc << std::endl;
     std::cout << "Here's a number: " << x << std::endl;
 }
