090b3f87c6978bafd53f0265be539c56718998b2
Kevin Robert Stravers <kevin@stravers.net>
2025-07-04 18:32:12 -0400

    Refactor declaration

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    int x = 0;
     std::cout << "Hello world" << std::endl;
+    int x = 0;
     x += 1;
     std::cout << "Here's a number: " << x << std::endl;
 }
